7b7dff882a6b4889c6305e72d948031204f78226
AGBEHONOU Yann <yann-prince-djilan.agbehonou@etu.univ-lehavre.fr>
2026-06-02 14:25:52 +0200

    Mise a jour de l'entite RealEstate pour ajouter les champs is_coup_de_coeur et is_destination_populaire

--- src/Entity/RealEstate.php
+++ src/Entity/RealEstate.php
@@ -12,67 +12,67 @@ use Gedmo\Mapping\Annotation as Gedmo;
 #[ORM\Entity(repositoryClass: RealEstateRepository::class)]
 class RealEstate
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column]
+	private ?int $id = null;
 
-    #[ORM\Column(length: 100)]
-    private ?string $title = null;
+	#[ORM\Column(length: 100)]
+	private ?string $title = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $description = null;
+	#[ORM\Column(type: Types::TEXT)]
+	private ?string $description = null;
 
-    #[ORM\Column(length: 200)]
-    private ?string $promotion = null;
+	#[ORM\Column(length: 200)]
+	private ?string $promotion = null;
 
-    #[ORM\Column(length: 150)]
-    private ?string $slug = null;
+	#[ORM\Column(length: 150)]
+	private ?string $slug = null;
 
-    #[ORM\Column]
-    private ?float $price = null;
+	#[ORM\Column]
+	private ?float $price = null;
 
-    #[ORM\Column]
-    private ?int $max_travelers = null;
+	#[ORM\Column]
+	private ?int $max_travelers = null;
 
-    #[ORM\Column]
-    private ?int $adults = null;
+	#[ORM\Column]
+	private ?int $adults = null;
 
-    #[ORM\Column]
-    private ?int $children = null;
+	#[ORM\Column]
+	private ?int $children = null;
 
-    #[ORM\Column]
-    private ?int $babies = null;
+	#[ORM\Column]
+	private ?int $babies = null;
 
-    #[ORM\Column(nullable: true)]
-    private ?int $likes = null;
+	#[ORM\Column(nullable: true)]
+	private ?int $likes = null;
 
-    #[ORM\Column(length: 50)]
-    private ?string $streetNumber = null;
+	#[ORM\Column(length: 50)]
+	private ?string $streetNumber = null;
 
-    #[ORM\Column(length: 150)]
-    private ?string $streetName = null;
+	#[ORM\Column(length: 150)]
+	private ?string $streetName = null;
 
-    #[ORM\Column(length: 100)]
-    private ?string $postalCode = null;
+	#[ORM\Column(length: 100)]
+	private ?string $postalCode = null;
 
-    #[ORM\Column(length: 50, nullable: true)]
-    private ?string $addressLine2 = null;
+	#[ORM\Column(length: 50, nullable: true)]
+	private ?string $addressLine2 = null;
 
-    #[ORM\Column(length: 150)]
-    private ?string $city = null;
+	#[ORM\Column(length: 150)]
+	private ?string $city = null;
 
-    #[ORM\Column(length: 150)]
-    private ?string $country = null;
+	#[ORM\Column(length: 150)]
+	private ?string $country = null;
 
-    #[ORM\Column]
-    private ?float $longitude = null;
+	#[ORM\Column]
+	private ?float $longitude = null;
 
-    #[ORM\Column]
-    private ?float $latitude = null;
+	#[ORM\Column]
+	private ?float $latitude = null;
 
-    #[ORM\Column]
-    private ?bool $is_online = null;
+	#[ORM\Column]
+	private ?bool $is_online = null;
 
 	#[Gedmo\Timestampable(on: 'create')]
 	#[ORM\Column]
@@ -82,23 +82,29 @@ class RealEstate
 	#[ORM\Column]
 	private ?\DateTimeImmutable $updated_at = null;
 
-    #[ORM\ManyToOne(inversedBy: 'realEstate')]
-    private ?Agenda $agenda = null;
+	#[ORM\ManyToOne(inversedBy: 'realEstate')]
+	private ?Agenda $agenda = null;
 
-    /**
-     * @var Collection<int, Image>
-     */
-    #[ORM\OneToMany(targetEntity: Image::class, mappedBy: 'realEstate')]
-    private Collection $images;
+	/**
+	 * @var Collection<int, Image>
+	 */
+	#[ORM\OneToMany(targetEntity: Image::class, mappedBy: 'realEstate')]
+	private Collection $images;
 
-    #[ORM\ManyToOne(inversedBy: 'realEstate')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Categorie $categorie = null;
+	#[ORM\ManyToOne(inversedBy: 'realEstate')]
+	#[ORM\JoinColumn(nullable: false)]
+	private ?Categorie $categorie = null;
 
-    public function __construct()
-    {
-        $this->images = new ArrayCollection();
-    }
+	#[ORM\Column(options: ['default' => false])]
+	private bool $is_coup_de_coeur = false;
+
+	#[ORM\Column(options: ['default' => false])]
+	private bool $is_destination_populaire = false;
+
+	public function __construct()
+	{
+		$this->images = new ArrayCollection();
+	}
 
 	public function __toString(): string
 	{
@@ -107,314 +113,336 @@ class RealEstate
 
 
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): static
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getPromotion(): ?string
-    {
-        return $this->promotion;
-    }
-
-    public function setPromotion(string $promotion): static
-    {
-        $this->promotion = $promotion;
-
-        return $this;
-    }
-
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-
-    public function setSlug(string $slug): static
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
-
-    public function getPrice(): ?float
-    {
-        return $this->price;
-    }
-
-    public function setPrice(float $price): static
-    {
-        $this->price = $price;
-
-        return $this;
-    }
-
-    public function getMaxTravelers(): ?int
-    {
-        return $this->max_travelers;
-    }
-
-    public function setMaxTravelers(int $max_travelers): static
-    {
-        $this->max_travelers = $max_travelers;
-
-        return $this;
-    }
-
-    public function getAdults(): ?int
-    {
-        return $this->adults;
-    }
-
-    public function setAdults(int $adults): static
-    {
-        $this->adults = $adults;
-
-        return $this;
-    }
-
-    public function getChildren(): ?int
-    {
-        return $this->children;
-    }
-
-    public function setChildren(int $children): static
-    {
-        $this->children = $children;
-
-        return $this;
-    }
-
-    public function getBabies(): ?int
-    {
-        return $this->babies;
-    }
-
-    public function setBabies(int $babies): static
-    {
-        $this->babies = $babies;
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
+
+	public function getTitle(): ?string
+	{
+		return $this->title;
+	}
+
+	public function setTitle(string $title): static
+	{
+		$this->title = $title;
+
+		return $this;
+	}
+
+	public function getDescription(): ?string
+	{
+		return $this->description;
+	}
+
+	public function setDescription(string $description): static
+	{
+		$this->description = $description;
+
+		return $this;
+	}
+
+	public function getPromotion(): ?string
+	{
+		return $this->promotion;
+	}
+
+	public function setPromotion(string $promotion): static
+	{
+		$this->promotion = $promotion;
+
+		return $this;
+	}
+
+	public function getSlug(): ?string
+	{
+		return $this->slug;
+	}
+
+	public function setSlug(string $slug): static
+	{
+		$this->slug = $slug;
+
+		return $this;
+	}
+
+	public function getPrice(): ?float
+	{
+		return $this->price;
+	}
+
+	public function setPrice(float $price): static
+	{
+		$this->price = $price;
+
+		return $this;
+	}
+
+	public function getMaxTravelers(): ?int
+	{
+		return $this->max_travelers;
+	}
+
+	public function setMaxTravelers(int $max_travelers): static
+	{
+		$this->max_travelers = $max_travelers;
+
+		return $this;
+	}
+
+	public function getAdults(): ?int
+	{
+		return $this->adults;
+	}
+
+	public function setAdults(int $adults): static
+	{
+		$this->adults = $adults;
+
+		return $this;
+	}
+
+	public function getChildren(): ?int
+	{
+		return $this->children;
+	}
+
+	public function setChildren(int $children): static
+	{
+		$this->children = $children;
+
+		return $this;
+	}
+
+	public function getBabies(): ?int
+	{
+		return $this->babies;
+	}
+
+	public function setBabies(int $babies): static
+	{
+		$this->babies = $babies;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getLikes(): ?int
-    {
-        return $this->likes;
-    }
-
-    public function setLikes(?int $likes): static
-    {
-        $this->likes = $likes;
+	public function getLikes(): ?int
+	{
+		return $this->likes;
+	}
+
+	public function setLikes(?int $likes): static
+	{
+		$this->likes = $likes;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getStreetNumber(): ?string
-    {
-        return $this->streetNumber;
-    }
-
-    public function setStreetNumber(string $streetNumber): static
-    {
-        $this->streetNumber = $streetNumber;
+	public function getStreetNumber(): ?string
+	{
+		return $this->streetNumber;
+	}
+
+	public function setStreetNumber(string $streetNumber): static
+	{
+		$this->streetNumber = $streetNumber;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getStreetName(): ?string
-    {
-        return $this->streetName;
-    }
-
-    public function setStreetName(string $streetName): static
-    {
-        $this->streetName = $streetName;
+	public function getStreetName(): ?string
+	{
+		return $this->streetName;
+	}
+
+	public function setStreetName(string $streetName): static
+	{
+		$this->streetName = $streetName;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPostalCode(): ?string
-    {
-        return $this->postalCode;
-    }
-
-    public function setPostalCode(string $postalCode): static
-    {
-        $this->postalCode = $postalCode;
+	public function getPostalCode(): ?string
+	{
+		return $this->postalCode;
+	}
+
+	public function setPostalCode(string $postalCode): static
+	{
+		$this->postalCode = $postalCode;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getAddressLine2(): ?string
-    {
-        return $this->addressLine2;
-    }
-
-    public function setAddressLine2(?string $addressLine2): static
-    {
-        $this->addressLine2 = $addressLine2;
+	public function getAddressLine2(): ?string
+	{
+		return $this->addressLine2;
+	}
+
+	public function setAddressLine2(?string $addressLine2): static
+	{
+		$this->addressLine2 = $addressLine2;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): static
-    {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    public function getCountry(): ?string
-    {
-        return $this->country;
-    }
-
-    public function setCountry(string $country): static
-    {
-        $this->country = $country;
-
-        return $this;
-    }
-
-    public function getLongitude(): ?float
-    {
-        return $this->longitude;
-    }
-
-    public function setLongitude(float $longitude): static
-    {
-        $this->longitude = $longitude;
-
-        return $this;
-    }
-
-    public function getLatitude(): ?float
-    {
-        return $this->latitude;
-    }
-
-    public function setLatitude(float $latitude): static
-    {
-        $this->latitude = $latitude;
-
-        return $this;
-    }
-
-    public function isOnline(): ?bool
-    {
-        return $this->is_online;
-    }
-
-    public function setIsOnline(bool $is_online): static
-    {
-        $this->is_online = $is_online;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->created_at;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $created_at): static
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updated_at;
-    }
-
-    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
-    {
-        $this->updated_at = $updated_at;
-
-        return $this;
-    }
-
-    public function getAgenda(): ?Agenda
-    {
-        return $this->agenda;
-    }
-
-    public function setAgenda(?Agenda $agenda): static
-    {
-        $this->agenda = $agenda;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Image>
-     */
-    public function getImages(): Collection
-    {
-        return $this->images;
-    }
-
-    public function addImage(Image $image): static
-    {
-        if (!$this->images->contains($image)) {
-            $this->images->add($image);
-            $image->setRealEstate($this);
-        }
-
-        return $this;
-    }
-
-    public function removeImage(Image $image): static
-    {
-        if ($this->images->removeElement($image)) {
-            // set the owning side to null (unless already changed)
-            if ($image->getRealEstate() === $this) {
-                $image->setRealEstate(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getCategorie(): ?Categorie
-    {
-        return $this->categorie;
-    }
-
-    public function setCategorie(?Categorie $categorie): static
-    {
-        $this->categorie = $categorie;
-
-        return $this;
-    }
+	public function getCity(): ?string
+	{
+		return $this->city;
+	}
+
+	public function setCity(string $city): static
+	{
+		$this->city = $city;
+
+		return $this;
+	}
+
+	public function getCountry(): ?string
+	{
+		return $this->country;
+	}
+
+	public function setCountry(string $country): static
+	{
+		$this->country = $country;
+
+		return $this;
+	}
+
+	public function getLongitude(): ?float
+	{
+		return $this->longitude;
+	}
+
+	public function setLongitude(float $longitude): static
+	{
+		$this->longitude = $longitude;
+
+		return $this;
+	}
+
+	public function getLatitude(): ?float
+	{
+		return $this->latitude;
+	}
+
+	public function setLatitude(float $latitude): static
+	{
+		$this->latitude = $latitude;
+
+		return $this;
+	}
+
+	public function isOnline(): ?bool
+	{
+		return $this->is_online;
+	}
+
+	public function setIsOnline(bool $is_online): static
+	{
+		$this->is_online = $is_online;
+
+		return $this;
+	}
+
+	public function getCreatedAt(): ?\DateTimeImmutable
+	{
+		return $this->created_at;
+	}
+
+	public function setCreatedAt(\DateTimeImmutable $created_at): static
+	{
+		$this->created_at = $created_at;
+
+		return $this;
+	}
+
+	public function getUpdatedAt(): ?\DateTimeImmutable
+	{
+		return $this->updated_at;
+	}
+
+	public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+	{
+		$this->updated_at = $updated_at;
+
+		return $this;
+	}
+
+	public function getAgenda(): ?Agenda
+	{
+		return $this->agenda;
+	}
+
+	public function setAgenda(?Agenda $agenda): static
+	{
+		$this->agenda = $agenda;
+
+		return $this;
+	}
+
+	/**
+	 * @return Collection<int, Image>
+	 */
+	public function getImages(): Collection
+	{
+		return $this->images;
+	}
+
+	public function addImage(Image $image): static
+	{
+		if (!$this->images->contains($image)) {
+			$this->images->add($image);
+			$image->setRealEstate($this);
+		}
+
+		return $this;
+	}
+
+	public function removeImage(Image $image): static
+	{
+		if ($this->images->removeElement($image)) {
+			// set the owning side to null (unless already changed)
+			if ($image->getRealEstate() === $this) {
+				$image->setRealEstate(null);
+			}
+		}
+
+		return $this;
+	}
+
+	public function getCategorie(): ?Categorie
+	{
+		return $this->categorie;
+	}
+
+	public function setCategorie(?Categorie $categorie): static
+	{
+		$this->categorie = $categorie;
+
+		return $this;
+	}
+
+	public function isCoupDeCoeur(): bool
+	{
+		return $this->is_coup_de_coeur;
+	}
+
+	public function setIsCoupDeCoeur(bool $is_coup_de_coeur): static
+	{
+		$this->is_coup_de_coeur = $is_coup_de_coeur;
+		return $this;
+	}
+
+	public function isDestinationPopulaire(): bool
+	{
+		return $this->is_destination_populaire;
+	}
+
+	public function setIsDestinationPopulaire(bool $is_destination_populaire): static
+	{
+		$this->is_destination_populaire = $is_destination_populaire;
+		return $this;
+	}
 }
