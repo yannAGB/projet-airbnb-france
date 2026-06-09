7881c12217f9ab4d32e43b0ce4ce049c1a309094
AGBEHONOU Yann <yann-prince-djilan.agbehonou@etu.univ-lehavre.fr>
2026-06-09 15:21:45 +0200

    Mise a jour des Fixtures de l'entite Like

--- src/DataFixtures/LikeFixtures.php
+++ src/DataFixtures/LikeFixtures.php
@@ -3,16 +3,21 @@
 namespace App\DataFixtures;
 
 use App\Entity\Like;
+use App\Entity\RealEstate;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
-class LikeFixtures extends Fixture implements FixtureGroupInterface
+class LikeFixtures extends Fixture implements FixtureGroupInterface, DependentFixtureInterface
 {
-    public static function getGroups(): array
+	public static function getGroups(): array { return ['likes']; }
+
+	public function getDependencies(): array
     {
-        return ['likes'];
+        return [UserFixtures::class, RealEstateFixtures::class];
     }
 
     public function load(ObjectManager $manager): void
@@ -23,8 +28,10 @@ class LikeFixtures extends Fixture implements FixtureGroupInterface
 
             $like = new Like();
 
-            $like->setReview(rand(1, 500));
-            $like->setComment($faker->optional()->sentence());
+            $like->setReview(rand(3, 5));
+            $like->setComment($faker->boolean(70) ? $faker->sentence(12) : null);
+			$like->setRealEstate($this->getReference('real-estate-' . rand(0, 79), RealEstate::class));
+			$like->setReviewer($this->getReference('user-' . rand(0, 39), User::class));
             $like->setCreatedAt(\DateTimeImmutable::createFromMutable(
                 $faker->dateTimeBetween('-1 year')
             ));
